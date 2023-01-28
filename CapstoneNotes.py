01f1a5c8dcc452b34cc87838c5c1ad5da7cf2b53
ghusak83 <husak@geog.ucsb.edu>
2023-01-27 16:00:12 -0800

    Add more code to visualize monly maximum rainfall.

--- CapstoneNotes.py
+++ CapstoneNotes.py
@@ -42,4 +42,22 @@ cb = plt.colorbar(janmap,cax=fig.add_axes([0.1,0.09,0.8,0.03]), orientation='hor
 
 #plt.savefig('testmap.png',dpi=200)
 
+#%% find the index of the maximum value
+maxind = clim.precip.argmax(dim="time")
+
+projection = ccrs.PlateCarree()  #set the projection of the map
+fig = plt.figure(figsize=(12,4))  #make the window for the graphics
+ax = plt.axes([0.05,0.15,0.9,0.8],projection=projection)  # set the drawing area within the window
+
+monmap = plt.pcolormesh(clim.longitude,clim.latitude,maxind,\
+                        vmin=-0.5, vmax=11.5, cmap='hsv')
+
+ax.set_title('Month of Maximum Rainfall')  #put a title on the map
+ax.coastlines(color='gray') #draw the coastlines in gray
+#ax.add_feature(count_bord,edgecolor='gray') #draw the country boundaries
+ax.add_feature(cartopy.feature.OCEAN,color='white',zorder=100) #color the oceans
+cb = plt.colorbar(monmap,cax=fig.add_axes([0.1,0.09,0.8,0.03]), orientation='horizontal',\
+                  ticks=[0,1,2,3,4,5,6,7,8,9,10,11],) #add colorbar
+cb.ax.set_xticklabels(['J','F','M','A','M','J','J','A','S','O','N','D'])
+
 
