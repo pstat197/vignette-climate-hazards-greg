354658a8250891e3465594135f74ea0eb5353645
ramirezgis <90123014+ramirezgis@users.noreply.github.com>
2023-02-11 18:37:23 -0800

    Add files via upload

---
+++ giselle/cleaner_ver_task.py
@@ -0,0 +1,160 @@
+#cleaner version
+#task: nail down wet vs dry areas based on seasonal precip
+#%% read in climatology
+import numpy as np
+import xarray as xr
+import cartopy.crs as ccrs
+import cartopy.feature
+import matplotlib.pyplot as plt
+from scipy.stats import percentileofscore
+import intake
+import holoviews as hv
+import cartopy.crs as ccrs
+import geoviews as gv
+import hvplot.xarray
+#%matplotlib inline
+import pandas as pd
+import pymannkendall as mk
+
+hv.extension('bokeh', width=80)
+count_bord = cartopy.feature.NaturalEarthFeature('cultural','admin_0_boundary_lines_land','110m',facecolor='none')
+#%% download here and graph it
+data_dir = 'C:/Users/gizra/OneDrive/Documents/netcdf/'
+infile = 'chirps-v2.0.monthly (1).nc'
+# %% 
+chirps = xr.open_dataset(data_dir+infile)
+#%% africa?
+#set spatial subset dimensions
+minlat = -40.; maxlat = 40.
+minlon = -20; maxlon = 50.
+CHsub =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
+CHsub.load()
+# %% drop first two months to create seasonal distribution
+drop_2mon = CHsub.drop([np.datetime64('1981-01-01'), np.datetime64('1981-02-01')], dim='time')
+#%% using rolling to see mean per season
+season_mean = drop_2mon.rolling(time=3).mean()#.dropna('time')
+# %%
+month_leng = drop_2mon.time.dt.days_in_month
+
+# %%
+# Calculate the weights by grouping by 'time.season'.
+weights = (
+    month_leng.groupby("time.season") / month_leng.groupby("time.season").sum()
+)
+
+# Test that the sum of the weights for each season is 1.0
+np.testing.assert_allclose(weights.groupby("time.season").sum().values, np.ones(4))
+
+# Calculate the weighted average
+ds_weighted = (drop_2mon * weights).groupby("time.season").sum(dim="time")
+# %%
+# only used for comparisons
+ds_unweighted = drop_2mon.groupby("time.season").mean("time")
+ds_diff = ds_weighted - ds_unweighted
+# %%
+# Quick plot to show the results
+notnull = pd.notnull(ds_unweighted["precip"][0])
+
+fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(14, 12))
+for i, season in enumerate(("DJF", "MAM", "JJA", "SON")):
+    ds_weighted["precip"].sel(season=season).where(notnull).plot.pcolormesh(
+        ax=axes[i, 0],
+        vmin=0,
+        vmax=300,
+        cmap="Spectral_r",
+        add_colorbar=True,
+        extend="both",
+    )
+
+    ds_unweighted["precip"].sel(season=season).where(notnull).plot.pcolormesh(
+        ax=axes[i, 1],
+        vmin=0,
+        vmax=300,
+        cmap="Spectral_r",
+        add_colorbar=True,
+        extend="both",
+    )
+
+    ds_diff["precip"].sel(season=season).where(notnull).plot.pcolormesh(
+        ax=axes[i, 2],
+        vmin=-0.1,
+        vmax=0.1,
+        cmap="RdBu_r",
+        add_colorbar=True,
+        extend="both",
+    )
+
+    axes[i, 0].set_ylabel(season)
+    axes[i, 1].set_ylabel("")
+    axes[i, 2].set_ylabel("")
+
+for ax in axes.flat:
+    ax.axes.get_xaxis().set_ticklabels([])
+    ax.axes.get_yaxis().set_ticklabels([])
+    ax.axes.axis("tight")
+    ax.set_xlabel("")
+
+axes[0, 0].set_title("Weighted by DPM")
+axes[0, 1].set_title("Equal Weighting")
+axes[0, 2].set_title("Difference")
+
+plt.tight_layout()
+
+fig.suptitle("Precipitation by Season", fontsize=16, y=1.02)
+# %%
+# select DJF
+DA_DJF = drop_2mon.sel(time=drop_2mon.time.dt.season=="DJF")
+
+# calculate mean per year
+DJF_mean = DA_DJF.groupby(DA_DJF.time.dt.year).mean("time")
+# %% plot DJF
+minlat = -40.; maxlat = 40.
+minlon = -20; maxlon = 50.
+#CHsub =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
+#CHsub.load()
+plot_djf = DJF_mean.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_djf.plot()
+t1= mk.seasonal_test(plot_djf, period = 42)
+# %% select mam
+DA_MAM = drop_2mon.sel(time=drop_2mon.time.dt.season=="MAM")
+
+# calculate mean per year
+MAM_mean = DA_MAM.groupby(DA_MAM.time.dt.year).mean("time")
+# %% plot DJF
+minlat = -40.; maxlat = 40.
+minlon = -20; maxlon = 50.
+#CHsub =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
+#CHsub.load()
+plot_mam = MAM_mean.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_mam.plot()
+t2= mk.seasonal_test(plot_mam, period = 42)
+
+# %%
+# select jja
+DA_JJA = drop_2mon.sel(time=drop_2mon.time.dt.season=="JJA")
+
+# calculate mean per year
+JJA_mean = DA_JJA.groupby(DA_JJA.time.dt.year).mean("time")
+# %% plot DJF
+minlat = -40.; maxlat = 40.
+minlon = -20; maxlon = 50.
+plot_jja = JJA_mean.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_jja.plot()
+t3= mk.seasonal_test(plot_jja, period = 42)
+# %% select son
+DA_SON = drop_2mon.sel(time=drop_2mon.time.dt.season=="SON")
+
+# calculate mean per year
+SON_mean = DA_SON.groupby(DA_SON.time.dt.year).mean("time")
+# %% plot DJF
+minlat = -40.; maxlat = 40.
+minlon = -20; maxlon = 50.
+#CHsub =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
+#CHsub.load()
+plot_son = SON_mean.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_son.plot()
+t4= mk.seasonal_test(plot_son, period = 42)
+
+# %% 25 only
+#AvgPrec = chirps.precip.mean("time")
+#AvgPrec25 = AvgPrec.where(AvgPrec.values <25)
---
+++ giselle/pre-task2.py
@@ -0,0 +1,176 @@
+#work for friday
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+#%% read in climatology
+import numpy as np
+import xarray as xr
+import cartopy.crs as ccrs
+import cartopy.feature
+import matplotlib.pyplot as plt
+from scipy.stats import percentileofscore
+count_bord = cartopy.feature.NaturalEarthFeature('cultural','admin_0_boundary_lines_land','110m',facecolor='none')
+#%% download here and graph it
+# get data from https://data.chc.ucsb.edu/products/CHPclim/netcdf/
+#"C:\Users\gizra\Downloads\chpclim.5050.monthly.nc"
+data_dir = 'C:/Users/gizra/OneDrive/Documents/netcdf/'#'/home/chc-data-out/products/CHPclim/netcdf/'
+infile = 'chirps-v2.0.monthly.nc'
+
+#notes:
+#moving metric of variablity (50 yr: increasing)
+
+chirps = xr.open_dataset(data_dir+infile)
+
+#make a quick visual of the january average rainfall
+#chirps.precip[0,:,:].plot.pcolormesh()
+
+#%%
+#janmap = plt.pcolormesh(clim.longitude,clim.latitude,clim.precip[0,:,:],\
+                        #vmin=0, vmax=400, cmap='PuBuGn')
+
+#janmap = plt.pcolormesh(clim.longitude,clim.latitude,clim.precip[0,:,:],\
+                        #vmin=0, vmax=400, cmap='PuBuGn')
+# %%
+#calculate percentile of rainfall
+perprec = chirps.precip.mean("time")
+perprec.plot.hist()
+
+#%%
+print(chirps['precip'].shape) #(504, 2000, 7200)
+
+# %%
+chirps["precip"].head()
+#latitude: -49.975 to 49.975
+#longitude: -180 to 180 (-179.975 to 179.975)
+
+
+# %%
+print("\n50th Percentile of arr, axis = 0(row) : ",
+      np.percentile(chirps["precip"], 50, axis=1))
+# %%
+#testing percentile
+my_array1 = [[[1, 3, 7, 8, 2], [3, 5, 1, 7, 9]], 
+[[9, 3, 4, 7, 1], [4, 5, 8, 6, 2]], 
+[[2, 3, 5, 6, 1], [5, 4, 7, 8, 9]]]
+print(np.percentile(my_array1, 50, axis=1))
+
+#output: [[2.  4.  4.  7.5 5.5]
+#[6.5 4.  6.  6.5 1.5]
+#[3.5 3.5 6.  7.  5. ]]
+#%%
+a = len(my_array1) #to find how many groups
+b = len(my_array1[1]) #2: number of groups within [[a], [b]] a+b= 2
+c = len(my_array1[1][1]) #5: number of items in each array
+
+#%%
+for i in my_array1:
+    b1 = len(my_array1[i])
+    #k = #
+    #c1 = len(my_array1[i][k])
+
+#my_new = my_array1.reshape(3, 2*5)
+# %%
+#function for 3d array
+
+#scores_d = scores.reshape(x*y,1)
+
+#percentiles_d = [percentileofscore(d[:, i], scores_d[i]) for i in range(x*y)]
+#percentiles_d = np.round(np.array(percentiles_d), 2).reshape(x,y)
+#print(percentiles_d)
+
+#%%
+data = np.array([[[ 1.,  1.,  1.],
+[ 1.,  1.,  1.],
+[ 1.,  1.,  1.]],
+   [[ 2.,  2.,  2.],
+    [ 2.,  2.,  2.],
+    [ 2.,  2.,  2.]],
+   [[ 3.,  3.,  3.],
+    [ 3.,  3.,  3.],
+    [ 3.,  3.,  3.]]])
+# %%
+x = 3 #rows
+y = 3 #columns? per row
+z = 3 #groups
+#x*y = items in the group
+
+d = data.reshape(z, x*y) #z
+#scores_d = scores.reshape(x*y,1)
+# %%
+#ds['temp'].sel(lon=65.5, lat=29.5) select coordinates
+#30.5595° S, 22.9375° E (south africa)
+#latitude, longitude
+#s_africa = chirps['precip'].sel('longitude'==30.5595, 'latitude'==22.9375)
+#s_africa = chirps.precip.sel(space='latitude')
+namibia = chirps.precip[:,12:20,17:28] #get precip
+namibia_mean = namibia.mean()
+#coefficient of variation, and duration of time when wet (must define on own)
+#nail down wet and dry (consider seasons); changes in variability or rainfall
+#trends: "areas getting drier" or opp. 
+#peak rainfall more variable
+#classification: (D/W) does it make sense? (2/3: 
+# ideas: always dry (1)? outliers
+# )
+
+#$$c(E 12°33'00"--E 19°39'00"/S 17°33'00"--S 27°42'00")
+# %%
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#data = pd.read_csv('path_to_file/stock.csv')
+#df = pd.DataFrame(data, columns = ['ValueDate', 'Price'])
+
+# Set the Date as Index
+#df['ValueDate'] = pd.to_datetime(df['ValueDate'])
+#df.index = df['ValueDate']
+#del df['ValueDate']
+#plot1 = chirps.isel('latitude' == [12:20], 'longitude':[12:28])
+data = pd.read_csv('')
+chirps.precip[12:20,17:28].plot.line("b-^")
+namibia.plot(figsize=(12,4))
+chirps['precip'].isel(longitude = 17, latitude = [12, 13, 14, 15, 16, 17, 18, 19, 20]).plot.line(x="time")
+
+#df.plot(figsize=(15, 6))
+plt.show()
+# %%
+#inch or more -- 4 months (25mm)
+#absolute amount (as shown in slides)
+random1 = chirps.precip[0, 12, 17]
+random1.head()
+
+#%%
+
+#set spatial subset dimensions
+minlat = -35.; maxlat = 0.
+minlon = 5.; maxlon = 50.
+CHsub =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
+CHsub.load()
+
+CHsub.precip[0,:,:].plot.pcolormesh()
+
+
+# %%
+minlat = -35.; maxlat = 0.
+minlon = 5.; maxlon = 50.
+minprec = 0.; maxprec = 25.
+CHsub =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
+
+CHsub.load()
+
+#inch or more -- 4 months (25mm)
+# %%
+chirps_three = chirps.precip[0:3,:,:]
+#test_where = chirps_three.where((precip))
+
+# %%
+
+#
+# small country (slicing)
+#1. find avg
+#ThrMA = clim.rolling(time=3).mean().dropna('time')
+#
