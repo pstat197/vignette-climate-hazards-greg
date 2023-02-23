05504a1c75380f79fc7a683d371c49046ac518b6
Giselle Ramirez <ramirezgis1225@gmail.com>
2023-02-23 00:36:47 -0800

    update acf, pacf for dem rep of congo plus congo

--- giselle/autocorrelation_draft.py
+++ giselle/autocorrelation_draft.py
@@ -18,6 +18,10 @@ from statsmodels.graphics import tsaplots
 import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from matplotlib import pyplot
+import warnings
+import itertools
+import matplotlib
+import pmdarima as pm
 
 
 hv.extension('bokeh', width=80)
@@ -30,13 +34,105 @@ chirps = xr.open_dataset(data_dir+infile)
 #%%
 #congo: #c(E 16°48'00"--E 29°25'00"/N 5°31'00"--S 11°55'00")
 #did I get coordinates right?
-minlat = -17.; maxlat = 28.
-minlon = 12; maxlon = 20.
+minlat = -17.; maxlat = 5.
+minlon = 12; maxlon = 32.
 congo =  chirps.sel(latitude=slice(minlat,maxlat),longitude=slice(minlon,maxlon))
-congo.load()
+#congo.load()
+#congo.precip[0,:,:].plot.pcolormesh()
 
 # %% seasonal time
 drop_2mon = congo.drop([np.datetime64('1981-01-01'), np.datetime64('1981-02-01')], dim='time')
+mean_seasonal = drop_2mon.rolling(time=3).mean()
+#%%
+seasonal_mean2_all = congo.groupby(congo.time.dt.year).mean('time')
+#%%
+minlat = -17.; maxlat = 5.
+minlon = 12; maxlon = 32.
+plot_month2 = mean_seasonal.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_month = seasonal_mean2_all.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_month3 = congo.mean(dim=["longitude", "latitude"]).to_pandas()
+plot_month3.plot()
+#t0= mk.seasonal_test(plot_month, period = 40)
+##don't run below!!!!
+#%% SARIMA 
+congo_reset = plot_month3.reset_index()
+ts_congo = congo_reset[['time', 'precip']]
+ts_congo['time'] = pd.to_datetime(ts_congo['time'])
+ts_congo = ts_congo.dropna()
+ts_congo.isnull().sum()
+
+ts_congo = ts_congo.set_index('time')
+#%%
+ts_month_avg = ts_congo['precip'].resample('MS').mean()
+ts_month_avg.plot(figsize=(15,6))
+#%%
+from pylab import rcParams
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(ts_month_avg, model='additive')
+fig = decomposition.plot()
+plt.show()
+#%%
+from statsmodels.tsa.stattools import adfuller
+def adf_test(timeseries):
+    #Perform Dickey-Fuller test:
+    print ('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used',\
+    'Number of Observations Used'])
+    for key,value in dftest[4].items():
+       dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+print(adf_test(ts_month_avg))
+#%%
+ts_s_adj = ts_month_avg - ts_month_avg.shift(12)
+ts_s_adj = ts_s_adj.dropna()
+ts_s_adj.plot()
+#%%
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+plot_acf(ts_s_adj)
+matplotlib.pyplot.show()
+plot_pacf(ts_s_adj)
+matplotlib.pyplot.show()
+#%%
+p = range(0, 3)
+d = range(1,2)
+q = range(0, 3)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(y,
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+            results = mod.fit()
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+#%%
+#don't run below
+#%%
+ts_delhi = series_delhi[['Date','AQI']]
+#converting 'Date' column to type 'datetime' so that indexing can happen later
+ts_delhi['Date'] = pd.to_datetime(ts_delhi['Date'])
+ts_delhi.isnull().sum()
+ts_delhi = ts_delhi.dropna()
+ts_delhi.isnull().sum()
+
+ts_delhi = ts_delhi.set_index('Date')
+
+ts_month_avg = ts_delhi['AQI'].resample('MS').mean()
+ts_month_avg.plot(figsize = (15, 6))
+plt.show()
 # %%
 # select DJF
 DA_DJF = drop_2mon.sel(time=drop_2mon.time.dt.season=="DJF")
